5398f05c98811c35a7b0b4e9de61551c76d777f6
NopphasinSriburin <Nopphasin.s@kkumail.com>
2026-06-28 14:52:16 +0700

    New upload

--- backend-api/src/main/java/com/gadgetshop/backend_api/GlobalCorsFilter.java
+++ backend-api/src/main/java/com/gadgetshop/backend_api/GlobalCorsFilter.java
@@ -3,8 +3,6 @@ package com.gadgetshop.backend_api;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 //import org.springframework.stereotype.Component;
 
 import java.io.IOException;
